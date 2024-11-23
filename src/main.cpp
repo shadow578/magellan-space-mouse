9dc38cea352caca541a811a201730f8c51bb01cd
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-11-23 11:49:34 +0100

    only trigger star button binding when double-pressing it to avoid conflict with hardware usage of the button

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,12 @@
 
 // magellan axis calibration values, as reported by CalibrationUtil
 static const magellan_internal::axis_calibration_t cal = {
-  .x={-3775, 2173}, 
-  .y={-3900, 4037}, 
-  .z={-1682, 3122}, 
-  .u={-2466, 3537}, 
-  .v={-3939, 2002}, 
-  .w={-3839, 1691}
+    .x = {-3775, 2173},
+    .y = {-3900, 4037},
+    .z = {-1682, 3122},
+    .u = {-2466, 3537},
+    .v = {-3939, 2002},
+    .w = {-3839, 1691},
 };
 
 // correction factors applied to the values received from the Magellan
@@ -32,6 +32,9 @@ constexpr float u_correction = 1.0f;  // rotation around x axis
 constexpr float v_correction = 1.0f;  // rotation around y axis
 constexpr float w_correction = -1.0f; // rotation around z axis
 
+// how long to wait for a double press of the "*" button
+constexpr uint32_t STAR_BUTTON_DOUBLE_PRESS_TIMEOUT = 500; // ms
+
 // mapping of Magellan buttons to HIDSpaceMouse buttons
 static const HIDSpaceMouse::KnownButton button_mappings[magellan_internal::BUTTON_COUNT] = {
     HIDSpaceMouse::ONE,   // Key "1"
@@ -42,7 +45,7 @@ static const HIDSpaceMouse::KnownButton button_mappings[magellan_internal::BUTTO
     HIDSpaceMouse::TOP,   // Key "6"
     HIDSpaceMouse::RIGHT, // Key "7"
     HIDSpaceMouse::FRONT, // Key "8"
-    HIDSpaceMouse::MENU   // Key "*"
+    HIDSpaceMouse::MENU   // Key "*" (double press)
 };
 
 HIDSpaceMouse spaceMouse(&Serial); // debug output to USB serial port
@@ -52,8 +55,102 @@ MagellanParser magellan(&cal, &Serial);  // debug output to USB serial port
 MagellanCalibrationUtil calibration(&Serial, &magellan);
 #endif
 
-bool was_ready = false;
-bool old_led = false;
+void handle_buttons(const bool from_event)
+{
+  constexpr uint8_t STAR_BUTTON_ID = 8;
+
+  // update button states according to mapping
+  // only when called from a button event (a button actually changed)
+  if (from_event)
+  {
+    for (uint8_t i = 0; i < magellan_internal::BUTTON_COUNT; i++)
+    {
+      // skip the "*" button, it's handled separately below
+      if (i == STAR_BUTTON_ID)
+      {
+        continue;
+      }
+
+      spaceMouse.set_button(button_mappings[i], magellan.get_button(i));
+    }
+  }
+
+  // check if the "*" button is pressed, then released, and then pressed again within 500ms
+  // runs always, as it needs to handle the timing of the button presses
+  // thus, the button state needs to be handled manually
+  enum StarButtonState
+  {
+    Idle,         // wait for first press
+    FirstDown,    // button was down the first time
+    FirstRelease, // button was released after the first press. millis are recorded in star_first_release_millis
+    SecondDown    // button was pressed again. if not within 500ms, go back to Idle
+  };
+
+  static StarButtonState star_button_state = Idle;
+  static uint32_t star_first_release_millis = 0;
+
+  const uint32_t now = millis();
+  const StarButtonState old_state = star_button_state;
+
+  switch (star_button_state)
+  {
+  case Idle:
+  {
+    if (magellan.get_button(STAR_BUTTON_ID))
+    {
+      star_button_state = FirstDown;
+    }
+    break;
+  }
+  case FirstDown:
+  {
+    if (!magellan.get_button(STAR_BUTTON_ID))
+    {
+      star_button_state = FirstRelease;
+      star_first_release_millis = now;
+    }
+    break;
+  }
+  case FirstRelease:
+  {
+    if (magellan.get_button(STAR_BUTTON_ID))
+    {
+      // button was pressed a second time
+      spaceMouse.set_button(button_mappings[STAR_BUTTON_ID], true);
+      star_button_state = SecondDown;
+    }
+
+    if ((now - star_first_release_millis) > STAR_BUTTON_DOUBLE_PRESS_TIMEOUT)
+    {
+      star_button_state = Idle;
+    }
+    break;
+  }
+  case SecondDown:
+  {
+    if (!magellan.get_button(STAR_BUTTON_ID))
+    {
+      // button was released
+      spaceMouse.set_button(button_mappings[STAR_BUTTON_ID], false);
+      star_button_state = Idle;
+    }
+    break;
+  }
+  default:
+  {
+    star_button_state = Idle;
+    break;
+  }
+  }
+
+  if (star_button_state != old_state)
+  {
+    Serial.print(F("[Main] STAR button state changed: "));
+    Serial.print(old_state);
+    Serial.print(F(" -> "));
+    Serial.println(star_button_state);
+  }
+}
 
 void setup()
 {
@@ -87,6 +184,7 @@ void loop()
   if (did_update)
   {
     const bool is_ready = magellan.ready();
+    static bool was_ready = false;
     if (is_ready && !was_ready)
     {
       // just became ready
@@ -112,11 +210,8 @@ void loop()
           magellan.get_v() * v_correction,
           magellan.get_w() * w_correction);
 
-      // update button states according to mapping
-      for (uint8_t i = 0; i < magellan_internal::BUTTON_COUNT; i++)
-      {
-        spaceMouse.set_button(button_mappings[i], magellan.get_button(i));
-      }
+      // update button states
+      handle_buttons(true);
     }
 
     // debug print to console even when not ready
@@ -137,8 +232,11 @@ void loop()
     Serial.println();
   }
 
+  handle_buttons(false);
+
   spaceMouse.update();
 
+  static bool old_led = false;
   if (spaceMouse.get_led() != old_led)
   {
     Serial.print("[Main] LED state changed: ");
